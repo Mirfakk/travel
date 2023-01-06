c2af894afe8ab7783e9045c8772b61fc1ab6a78c
Mirfakk <2286966430@qq.com>
2023-01-06 21:06:37 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//解决移动端300毫秒点击延迟问题
+import fastclick from 'fastclick'
+//引入样式
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+
+
 
 Vue.config.productionTip = false
+//fastClick自带的方法，解决300毫秒点击延迟问题
+fastclick.attach(document.body);
 
-/* eslint-disable no-new */
+/* 定义了vue的根实例，挂载到id=app的元素上*/
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
 })
+
+/*路由就是根据网址的不同，返回不同的内容给用户 */
